abd29827a86074f876842a933442330526daab6d
TerrestrialTechnologies <mygymus@gmail.com>
2026-07-14 00:46:50 -0400

    Insert sample data into files and owners tables

---
+++ insert_data.sql
@@ -0,0 +1,11 @@
+use GavinDB; 
+
+insert into files values
+('NotAVirus.exe', 696969, FALSE), 
+('DefinitelyVirus.wad', 9000, TRUE), 
+('TotallySafe.zip', 1200, FALSE);
+
+insert into owners values
+(1, 'NotAVirus.exe', 'Terrestrial'), 
+(2, 'TotallySafe.rar', 'Dem0noid'), 
+(3, 'GhostFile.zip', 'DreadPirateR0berts');
