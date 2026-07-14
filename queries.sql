99b109b195908ee4d230601e60071bcc1b1e52f2
TerrestrialTechnologies <mygymus@gmail.com>
2026-07-14 00:55:38 -0400

    Add multiple SQL queries for file management
    Added various SQL queries for updates, deletes, selects, joins, and aggregations.

--- queries.sql
+++ queries.sql
@@ -4,4 +4,69 @@ Select * from files;
 --update
 update files
 set sizekb = 60000
-where name = 'NotAVirus.exe'
+where name = 'NotAVirus.exe';
+
+--delete 
+delete from files
+where name = 'DefinitelyVirus.exe'; 
+
+-- and 
+select *
+from files
+where sizekb > 1000
+and virus = false; 
+
+--or
+select *
+from files 
+where virus = true
+or sizekb > 50000;
+
+--like 
+select * 
+form files
+where name like '%Virus%';
+
+--order by 
+select * 
+from files 
+order by sizekb desc; 
+
+--group by 
+select Virus, count(*)
+from files
+group by Virus;
+
+-- inner join 
+select files.name, owners.ownername
+from files
+inner join owners
+on files.name = owners.filename; 
+
+-- left join
+select files.name, owners.ownername
+from files
+left join owners
+on files.name = owners.filename; 
+
+-- right join 
+select files.name, owners.ownername
+from files
+right join owners
+on files.name = owners.filename; 
+
+--union
+select name 
+from files
+union 
+select ownername
+from owners; 
+
+--subquery 
+select * 
+from files 
+where sizekb > 
+(
+  select avg(sizekb)
+  from files
+);
